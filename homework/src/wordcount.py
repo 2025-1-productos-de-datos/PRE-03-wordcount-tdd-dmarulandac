6c07a40092a4b4f9d9d385a33a21d9d3e78d4236
Daniel Marulanda Cardona <dmarulandac@unal.edu.co>
2025-05-10 09:35:23 -0500

    FACT_PARSE_ARGS

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,22 @@
+## wordcount.py
 
+import os
+import sys
+
+from homework.src._internals.parse_args import parse_args
+
+
+def read_all_lines(input_folder):
+    lines=[]
+    for filename in os.listdir(input_folder):
+        file_path=os.path.join(input_folder,filename)
+        with open(file_path,"r",encoding="utf-8")as f:
+            lines.extend(f.readlines())
+    return lines
+
+
+
+
+def main():
+    input_folder, output_folder = parse_args()
+    lines=read_all_lines(input_folder)
